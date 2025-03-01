ef9032a19840807171f6cd407b7365863e15554a
blackcode2 <crija19@gmail.com>
2025-03-01 13:30:55 -0500

    options de notificacion por mail

---
+++ README.md
@@ -0,0 +1,5 @@
+-- 1 se crea carpeta para instalar con pnpm install playwright with (typecript o javascript)
+-- 2 tener node js instalado
+-- 3 instalar library playwright
+-- 4 instalar tempo library para parsear fechas
+-- 5 notificar por mail con mailjet
--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
+import { isAfter, parse } from '@formkit/tempo';
 import {chromium} from 'playwright';
+
+process.loadEnvFile();
 
 const browser = await chromium.launch();
 const page = await browser.newPage();
@@ -7,6 +10,49 @@ await page.goto("https://www.cgeonline.com.ar/informacion/apertura-de-citas.html
 
 const table = page.locator("table");
 const pasaporteRow = table.getByText("renovación y primera vez").locator("..").locator("..").locator("td:nth-child(3)")
-console.log(await pasaporteRow.innerText())
+const nextDate = await pasaporteRow.innerText();
+const nextDatePrueba = "05/03/2025 a las 09:00";
+const [date, time] = nextDatePrueba.split(" a las ");
+const parseDate = parse(`${date} ${time}`, "DD/MM/YYYY hh:mm");
+
+// console.log(`La proxima apertura de fechas para pasaportes es el: ${parseDate}`);
+
+if (isAfter(parseDate, new Date())) {
+
+  try {
+    await fetch("https://api.mailjet.com/v3.1/send", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        "Authorization": `Basic ${Buffer.from(`${process.env.MJ_PUBLIC_KEY}:${process.env.MJ_SECRET_KEY}`).toString("base64")}`
+      },
+      body: JSON.stringify({
+        SandboxMode: false,
+        Messagess: [
+          {
+            From: {
+              Email: "gonzalo.pozzo4@gmail.com",
+              Name: "Your Mailjet Pilot"
+            },
+            HTMLPart: `<h3>La proxima apertura de fechas para pasaportes es el: ${parseDate}</h3><br/>May the delivery force be with you! `,
+            Subjet: "Hay turno para pasaporte!",
+            TextPart: `La proxima apertura de fechas para pasaportes es el: ${parseDate}`,
+            To: [
+              {
+                Email: "crija19@outlook.es",
+                Name: "cristian 1"
+              }
+            ]
+          }
+        ]
+      })
+    })
+    
+  } catch (error) {
+    console.log(error);
+  }
+} else {
+  console.log(`No hay fehcas estipuladas`);
+}
 
 await browser.close();
